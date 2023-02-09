ccbf58bb646c18a09fd7da5ffb6bcc814fd7e98a
Oke Amuwha <amuwhao46@gmail.com>
2023-02-09 14:51:32 -0500

    Attempting to add text above bar

--- lib/horProgressBar.dart
+++ lib/horProgressBar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class HorProgressBar extends StatefulWidget {
@@ -13,14 +12,13 @@ class _HorProgressBar extends State<HorProgressBar> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      child: LinearPercentIndicator(
+            child: Column(
+      children: [LinearPercentIndicator(
         lineHeight: 20,
         percent: .4,
-        leading: new Text(
-            'Food'), // Will figure out how to put this as a header later
         progressColor: const Color.fromRGBO(34, 197, 94, 1),
         backgroundColor: const Color.fromRGBO(211, 211, 211, 1),
-      ),
-    ));
+      )],
+    )));
   }
 }
