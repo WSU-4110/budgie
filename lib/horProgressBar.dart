14ecc6e01d6c31df7ff0526d0d6476f9a64a579a
Oke Amuwha <amuwhao46@gmail.com>
2023-02-10 15:06:38 -0500

    Made progress bar and text dynamic

--- lib/horProgressBar.dart
+++ lib/horProgressBar.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
+
+String category = 'Food';
+String progressAmt = '100';
+String totalProgress = '500';
 
 class HorProgressBar extends StatefulWidget {
   const HorProgressBar({Key? key}) : super(key: key);
@@ -8,26 +13,36 @@ class HorProgressBar extends StatefulWidget {
   _HorProgressBar createState() => _HorProgressBar();
 }
 
-// Testing
-const String thing1 = 'Test';
-const String thing2 = '_Test';
-
 class _HorProgressBar extends State<HorProgressBar> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            child: Center(
-                child: Column(
-      children: [
-        const Text('Food'),
-        LinearPercentIndicator(
-          lineHeight: 20,
-          percent: .4,
-          progressColor: const Color.fromRGBO(34, 197, 94, 1),
-          backgroundColor: const Color.fromRGBO(211, 211, 211, 1),
-        )
-        const Text('300 0f 500'),
-      ],
-    ))));
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(category,
+              textAlign: TextAlign.left,
+              style:
+                  const TextStyle(fontWeight: FontWeight.w600, fontSize: 20)),
+          LinearPercentIndicator(
+            lineHeight: 20,
+            percent: percentHelper(progressAmt, totalProgress),
+            progressColor: const Color.fromRGBO(34, 197, 94, 1),
+            backgroundColor: const Color.fromRGBO(211, 211, 211, 1),
+          ),
+          Text('$progressAmt of $totalProgress', textAlign: TextAlign.left),
+        ],
+      )),
+    );
   }
+}
+
+// Calculates LinearPercentIndicator.percent
+double percentHelper(String dividend, String divisor) {
+  // Some text here
+  var intDividend = double.parse(dividend);
+  var intDivisor = double.parse(divisor);
+
+  return intDividend / intDivisor;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
       home: const HorProgressBar(),
     );
   }
