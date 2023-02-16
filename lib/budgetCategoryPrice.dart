529c8b6df94afbb7c5e2a65b738ea75576785562
Oke Amuwha <amuwhao46@gmail.com>
2023-02-16 17:48:33 -0500

    Preparing for compartmentalization

--- lib/budgetCategoryPrice.dart
+++ lib/budgetCategoryPrice.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class BudgetCategoryPrice {
-  int currentCategoryPrice = 0;
-  int totalCategoryPrice = 0;
+  String currentCategoryPrice = '';
+  String totalCategoryPrice = '';
+  String categoryName = '';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'horProgressBar.dart';
+import 'package:budgie/horProgressBar.dart';
 
 void main() {
   runApp(MyApp());
