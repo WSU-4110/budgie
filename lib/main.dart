0d764dc72a278a7309b70efffc3dc5607f0ecd55
Oke Amuwha <amuwhao46@gmail.com>
2023-03-06 20:54:14 -0500

    Added firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@ import 'package:budgie/button.dart';
 import 'package:flutter/material.dart';
 import 'horProgressBar.dart';
 import 'BudgetCircle/overallBudgetCircle.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
