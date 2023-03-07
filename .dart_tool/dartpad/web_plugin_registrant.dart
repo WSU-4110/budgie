9dacf72c125108dc00acdc4ed2465819fe0774b7
Oke Amuwha <amuwhao46@gmail.com>
2023-03-06 19:58:18 -0500

    Started integrating firebase

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -3,6 +3,14 @@
 // Generated file. Do not edit.
 //
 
+// @dart = 2.13
 // ignore_for_file: type=lint
 
-void registerPlugins() {}
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FirebaseCoreWeb.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
