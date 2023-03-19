38c5900ad61f6897f8b6c9cb147e22473e45404d
Manpreet Singh <mxsingh13@gmail.com>
2023-03-19 16:14:54 -0400

    Formatting

--- lib/articlesPage.dart
+++ lib/articlesPage.dart
@@ -17,40 +17,49 @@ class ArticlesPage extends StatelessWidget {
             return Center(
               child: Card(
                   child: Material(
-                color: Colors.green,
-                borderRadius: BorderRadius.circular(8.0),
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(8.0),
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const ExpandedArticle()));
-                  },
-                  child: Container(
-                      width: 200,
-                      height: 100,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8.0)),
-                      child: Center(
-                        child: ListView(
-                          physics: const NeverScrollableScrollPhysics(),
-                          padding: const EdgeInsets.all(20.0),
-                          shrinkWrap: true,
-                          children: [
-                            Text('Article #${index + 1}',
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white)),
-                            const Text('Information',
-                                style: TextStyle(color: Colors.white))
-                          ],
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(8.0),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const ExpandedArticle()
+                          )
+                        );
+                      },
+                      child: Container(
+                        width: 200,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8.0)
                         ),
-                      )),
+                        child: Center(
+                          child: ListView(
+                            physics: const NeverScrollableScrollPhysics(),
+                            padding: const EdgeInsets.all(20.0),
+                            shrinkWrap: true,
+                            children: [
+                              Text(
+                                'Article #${index + 1}',
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white
+                                )
+                              ),
+                              const Text('Information',
+                                style: TextStyle(color: Colors.white)
+                              )
+                            ],
+                          ),
+                        )
+                      ),
+                    ),
+                  )
                 ),
-              )),
-            );
-          }),
+              );
+            }),
         ),
       ),
     );
