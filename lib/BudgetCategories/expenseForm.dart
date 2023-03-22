2844a75575361a6bafa930e815d1b434be8d7325
Rey Jakosalem <105830872+Rey-Jey@users.noreply.github.com>
2023-03-22 02:09:47 -0400

    Initial Commit (flutter commands)

--- lib/BudgetCategories/expenseForm.dart
+++ lib/BudgetCategories/expenseForm.dart
@@ -3,9 +3,9 @@ import 'package:intl/intl.dart';
 
 class ExpenseForm extends StatefulWidget {
   const ExpenseForm({super.key});
-  
+
   @override
-  State<ExpenseForm> createState() =>  ExpenseFormState();
+  State<ExpenseForm> createState() => ExpenseFormState();
 }
 
 class ExpenseFormState extends State<ExpenseForm> {
@@ -32,84 +32,83 @@ class ExpenseFormState extends State<ExpenseForm> {
       key: _formKey,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget> [
-        TextFormField(
-          decoration: const InputDecoration(
-              icon: const Icon(Icons.assignment_outlined),    
-              hintText: 'Enter the Name of the Expense',  
-              labelText: 'Name',  
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              icon: const Icon(Icons.assignment_outlined),
+              hintText: 'Enter the Name of the Expense',
+              labelText: 'Name',
             ),
-            validator: (value) {  
-              if (value == null || value.isEmpty) {  
-                return 'Please enter some text';  
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
               }
-              return null;  
+              return null;
             },
-            controller: expenseName,   
-          ),  
-          TextFormField( 
-            keyboardType: TextInputType.number, 
-            decoration: const InputDecoration(  
-              icon: const Icon(Icons.attach_money_rounded),  
-              hintText: 'Enter the Cost',  
-              labelText: 'Cost', 
-            ),  
-            validator: (value) {  
-              if (value == null || value.isEmpty) {  
-                return 'Please enter a valid number';  
-              } 
-              return null;  
+            controller: expenseName,
+          ),
+          TextFormField(
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              icon: const Icon(Icons.attach_money_rounded),
+              hintText: 'Enter the Cost',
+              labelText: 'Cost',
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter a valid number';
+              }
+              return null;
             },
-            controller: expenseCost,   
-          ),  
-          TextFormField(  
-            decoration: const InputDecoration(  
-            icon: const Icon(Icons.calendar_today),  
-            hintText: 'Enter the date of the purchase',  
-            labelText: 'Select Date',  
-            ),  
-            onTap: () async{
+            controller: expenseCost,
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              icon: const Icon(Icons.calendar_today),
+              hintText: 'Enter the date of the purchase',
+              labelText: 'Select Date',
+            ),
+            onTap: () async {
               FocusScope.of(context).requestFocus(new FocusNode());
 
               DateTime? pickeddate = await showDatePicker(
-                context: context, 
-                initialDate: DateTime.now(),
-                firstDate: DateTime(2000),
-                lastDate: DateTime(2101)
-                );
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime(2101));
 
-                if (pickeddate != null) {
-                  setState(() {
-                    expenseDate.text = DateFormat.yMd().format(pickeddate);
-                  });
-                }
+              if (pickeddate != null) {
+                setState(() {
+                  expenseDate.text = DateFormat.yMd().format(pickeddate);
+                });
+              }
             },
-            validator: (value) {  
-              if (value == null || value.isEmpty) {  
-                return 'Please enter valid date';  
-              }  
-              return null;  
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter valid date';
+              }
+              return null;
             },
-            controller: expenseDate,   
-           ),  
-          new Container(  
-              padding: const EdgeInsets.only(left: 150.0, top: 40.0),  
-              child: new ElevatedButton(  
-                child: const Text('Submit'),  
+            controller: expenseDate,
+          ),
+          new Container(
+              padding: const EdgeInsets.only(left: 150.0, top: 40.0),
+              child: new ElevatedButton(
+                child: const Text('Submit'),
                 onPressed: () {
                   showDialog(
-                    context: context, 
+                    context: context,
                     builder: (context) {
-                      return AlertDialog(content: Text(expenseName.text + expenseCost.text + expenseDate.text));
+                      return AlertDialog(
+                          content: Text(expenseName.text +
+                              expenseCost.text +
+                              expenseDate.text));
                     },
                   );
-                },  
-              )),  
-        ],  
-      ),  
-    );  
-  }  
-}  
-
-
-
+                },
+              )),
+        ],
+      ),
+    );
+  }
+}
