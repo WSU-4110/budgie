ed3fdde9dbefe8bbffe8cd29c55915ad2184296f
Rey Jakosalem <reyjakosa@gmail.com>
2023-03-23 16:37:41 -0400

    Singleton Implementation for Overall Budget

--- lib/BudgetCircle/overallBudgetCircle.dart
+++ lib/BudgetCircle/overallBudgetCircle.dart
@@ -5,10 +5,14 @@ class OverBudgie extends StatefulWidget {
   const OverBudgie({Key? key}) : super(key: key);
 
   @override
-  _BudgetCircleState createState() => _BudgetCircleState();
+  _BudgetCircleState createState() => _BudgetCircleState.instance;
 }
 
 class _BudgetCircleState extends State<OverBudgie> {
+  //Singleton
+  _BudgetCircleState._();
+  static final instance = _BudgetCircleState._();
+
   double totalBudget = 600;
   double currentBudget = 90;
 
@@ -16,21 +20,23 @@ class _BudgetCircleState extends State<OverBudgie> {
 
   ovProgress(double totalBud, double currentBud) {
     double progress = 0;
-    
+
     if (totalBud <= 0) {
       return progress;
-    }
-
-    else{
+    } else {
       progress = (currentBud / totalBud);
       return progress;
     }
   }
 
   Color colorChange(double prog) {
-    if (prog <= .15 ){ return const Color.fromRGBO(225, 55, 43, 1); }
-    else if (prog <= .50) { return const Color.fromRGBO(255, 217, 102, 1); }
-    else { return const Color.fromRGBO(34, 197, 94, 1);}
+    if (prog <= .15) {
+      return const Color.fromRGBO(225, 55, 43, 1);
+    } else if (prog <= .50) {
+      return const Color.fromRGBO(255, 217, 102, 1);
+    } else {
+      return const Color.fromRGBO(34, 197, 94, 1);
+    }
   }
 
   Widget build(BuildContext context) {
@@ -42,14 +48,13 @@ class _BudgetCircleState extends State<OverBudgie> {
           lineWidth: 20,
           percent: ovProgress(totalBudget, currentBudget),
           center: new Text("\$" + currentBudgie,
-          style: 
-          new TextStyle(fontWeight: FontWeight.bold, fontSize: 40.0)
-          ),
+              style:
+                  new TextStyle(fontWeight: FontWeight.bold, fontSize: 40.0)),
           circularStrokeCap: CircularStrokeCap.round,
           backgroundColor: const Color.fromRGBO(211, 211, 211, 1),
           progressColor: colorChange(ovProgress(totalBudget, currentBudget)),
-          ),
         ),
-      );
+      ),
+    );
   }
 }
