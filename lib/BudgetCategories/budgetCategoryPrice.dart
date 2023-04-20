d92d391371bef2c04b92929facbb45b757528ec9
Rey Jakosalem <reyjakosa@gmail.com>
2023-04-20 03:27:57 -0400

    Moving cloud_get_expenses to firestore_cloud_service

--- lib/BudgetCategories/budgetCategoryPrice.dart
+++ lib/BudgetCategories/budgetCategoryPrice.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:budgie/cloud/cloud_get_expenses.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 
+String expenseSum = await getAllFieldValues(expenseFieldCost);
 String category = 'Food';
 String progressAmt = '400';
 String totalProgress = '500';
