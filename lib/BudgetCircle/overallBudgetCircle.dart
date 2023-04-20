5c58b35774725907209aef7d8670a91b98eea641
Rey Jakosalem <reyjakosa@gmail.com>
2023-04-19 20:17:23 -0400

    Firebase set up for budget categories

--- lib/BudgetCircle/overallBudgetCircle.dart
+++ lib/BudgetCircle/overallBudgetCircle.dart
@@ -1,46 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 
+ovProgress(double totalBud, double currentBud) {
+  double progress = 0;
+
+  if (totalBud <= 0) {
+    return progress;
+  } else {
+    progress = (currentBud / totalBud);
+    return progress;
+  }
+}
+
+Color colorChange(double prog) {
+  if (prog <= .15) {
+    return const Color.fromRGBO(225, 55, 43, 1);
+  } else if (prog <= .50) {
+    return const Color.fromRGBO(255, 217, 102, 1);
+  } else {
+    return const Color.fromRGBO(34, 197, 94, 1);
+  }
+}
+
 class OverBudgie extends StatefulWidget {
   const OverBudgie({Key? key}) : super(key: key);
 
   @override
-  _BudgetCircleState createState() => _BudgetCircleState.instance;
+  _BudgetCircleState createState() => _BudgetCircleState();
 }
 
 class _BudgetCircleState extends State<OverBudgie> {
-  //Singleton
-  _BudgetCircleState._();
-  static final instance = _BudgetCircleState._();
-
   double totalBudget = 600;
   double currentBudget = 90;
 
   late String currentBudgie = '$currentBudget';
   late String totalBudgie = '$totalBudget';
-
-  ovProgress(double totalBud, double currentBud) {
-    double progress = 0;
-
-
-    if (totalBud <= 0) {
-      return progress;
-    } else {
-    } else {
-      progress = (currentBud / totalBud);
-      return progress;
-    }
-  }
-
-  Color colorChange(double prog) {
-    if (prog <= .15) {
-      return const Color.fromRGBO(225, 55, 43, 1);
-    } else if (prog <= .50) {
-      return const Color.fromRGBO(255, 217, 102, 1);
-    } else {
-      return const Color.fromRGBO(34, 197, 94, 1);
-    }
-  }
 
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/cloud/cloud_field_constants.dart
+++ lib/cloud/cloud_field_constants.dart
@@ -1,3 +1,7 @@
 const expenseFieldName = 'name';
 const expenseFieldCost = 'cost';
 const expenseFieldDate = 'date';
+
+const budgetName = 'budget name';
+const budgetTotalCost = 'total';
+const budgetRemaining = 'remaining budget';
