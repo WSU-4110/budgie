2a0dbb54241e9508f02d2e5c487236873ee13bbe
Rey Jakosalem <reyjakosa@gmail.com>
2023-04-20 00:14:15 -0400

    Removing Budget Category Date

--- lib/BudgetButton/SecondPage.dart
+++ lib/BudgetButton/SecondPage.dart
@@ -26,42 +26,6 @@ class PageForName extends StatelessWidget {
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => PageForDate()),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class PageForDate extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Budget Date",
-              style: TextStyle(fontSize: 24.0),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Add the date of the Budget",
-              ),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              child: Text("Next"),
-              onPressed: () {
-                Navigator.push(
-                  context,
                   MaterialPageRoute(builder: (context) => PageForCost()),
                 );
               },
