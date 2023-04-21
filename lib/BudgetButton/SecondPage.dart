8b9ed85f929abcbf5cb89fc6f91a3db2dd511842
Rey Jakosalem <reyjakosa@gmail.com>
2023-04-21 07:13:03 -0400

    Changing SecondPage to interact with budget_form

--- lib/BudgetButton/SecondPage.dart
+++ lib/BudgetButton/SecondPage.dart
@@ -1,71 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:budgie/cloud/firestore_cloud_service.dart';
+import 'budget_form.dart';
 
 class PageForName extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Budget Name",
-              style: TextStyle(fontSize: 24.0),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Add the Budget Name",
-              ),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              child: Text("Next"),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => PageForCost()),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+  const PageForName({super.key});
 
-class PageForCost extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Budget Cost?",
-              style: TextStyle(fontSize: 24.0),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Add the cost of the budget",
-              ),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              child: Text("Done"),
-              onPressed: () {
-                Navigator.popUntil(context, ModalRoute.withName('/'));
-              },
-            ),
-          ],
+      appBar: AppBar(
+        leading: BackButton(
+          color: Colors.white,
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
+        backgroundColor: Colors.green,
+        title: const Text("Add a Budget"),
       ),
+      body: const Center(child: BudgetForm()),
     );
   }
 }
